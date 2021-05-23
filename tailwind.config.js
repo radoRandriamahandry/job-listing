05a010e0849cf347b5490efef89d8d3141221257
radoRandriamahandry <61495462+radoRandriamahandry@users.noreply.github.com>
2021-05-23 15:26:21 +0300

    initial project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./public/**/*.html", "./src/**/*.vue"],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
